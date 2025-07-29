95959926c42c2f97782de9ab26b7ea0acfbeb09d
I-Am-Cow <jakepalmersimmons@pm.me>
2025-07-29 16:40:50 +0100

    Add placeholder implementations for research components

---
+++ src/components/ArtistResearchFlow.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ArtistResearchFlow() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1>ArtistResearchFlow</h1>
+    </div>
+  )
+}
---
+++ src/components/ResearchHomepage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ResearchHomepage() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1>ResearchHomepage</h1>
+    </div>
+  )
+}
